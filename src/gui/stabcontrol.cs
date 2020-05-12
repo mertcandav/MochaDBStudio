9a2196c09950909a7a36d214797013320a8120ad
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-13 01:13:58 +0300

    Add tab control.

---
+++ src/gui/stabcontrol.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MochaDBStudio.gui {
+    /// <summary>
+    /// Tab control for MochaDB Studio.
+    /// </summary>
+    public sealed partial class stabcontrol:TabControl {
+        #region Fields
+
+        private int
+            lineX = 2;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public stabcontrol() {
+            Init();
+        }
+
+        #endregion
+
+        #region Drawing
+
+        protected override void OnPaintBackground(PaintEventArgs e) {
+            using(var bgbrush = new SolidBrush(BackColor))
+                e.Graphics.FillRectangle(bgbrush,ClientRectangle);
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            using(var centerFormat = new StringFormat() {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center
+            })
+            using(var forebrush = new SolidBrush(ForeColor))
+                for(short index = 0; index < TabCount; index++) {
+                    var tab = TabPages[index];
+                    var rect = GetRect(index);
+                    e.Graphics.DrawString(tab.Text,Font,forebrush,rect,centerFormat);
+                }
+
+            if(SelectedIndex != -1)
+                using(var pen = new Pen(Color.Gray,2)) {
+                    var rect = GetRect(SelectedIndex);
+                    e.Graphics.DrawLine(pen,lineX,rect.Y+rect.Height,lineX+(rect.Width),rect.Y+rect.Height);
+                }
+        }
+
+        #endregion
+
+        #region Item override
+
+        protected override void OnSelectedIndexChanged(EventArgs e) {
+            base.OnSelectedIndexChanged(e);
+            if(SelectedIndex != -1)
+                new Task(() => {
+                    var rect = GetRect(SelectedIndex);
+                    if(lineX == rect.X)
+                        return;
+
+                    if(rect.X <= lineX) {
+                        for(; lineX >= rect.X; lineX--) {
+                            Invalidate();
+                            Thread.Sleep(1);
+                        }
+                    } else {
+                        for(; lineX <= rect.X; lineX++) {
+                            Invalidate();
+                            Thread.Sleep(1);
+                        }
+                    }
+                }).Start();
+        }
+
+        #endregion
+
+        #region Mouse override
+
+        protected override void OnMouseClick(MouseEventArgs e) {
+            base.OnMouseClick(e);
+
+            var page = GetHoveringPage();
+            if(page != null) {
+                SelectedTab = page;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns tab rectangle by dex.
+        /// </summary>
+        /// <param name="index">Index of page.</param>
+        public Rectangle GetRect(int index) {
+            var rect = GetTabRect(index);
+            rect.X += rect.Width-60;
+            rect.Width = 70;
+            return rect;
+        }
+
+        /// <summary>
+        /// Return hovering page.
+        /// </summary>
+        public TabPage GetHoveringPage() {
+            for(int index = 0; index < TabCount; index++)
+                if(GetRect(index).Contains(PointToClient(Cursor.Position)))
+                    return TabPages[index];
+            return null;
+        }
+
+        /// <summary>
+        /// Return rectangle of hovering page.
+        /// </summary>
+        public Rectangle GetHoveringRect() {
+            for(int index = 0; index < TabCount; index++) {
+                Rectangle rect = GetRect(index);
+                if(rect.Contains(PointToClient(Cursor.Position)))
+                    return rect;
+            }
+            return new Rectangle();
+        }
+
+        /// <summary>
+        /// Add new tab page.
+        /// </summary>
+        /// <param name="text">Text of page.</param>
+        public void AddPage(string text) {
+            var page = new TabPage();
+            page.BackColor = BackColor;
+            TabPages.Add(page);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Background color.
+        /// </summary>
+        public new Color BackColor { get; set; }
+
+        /// <summary>
+        /// Foreground color.
+        /// </summary>
+        public new Color ForeColor { get; set; }
+
+        #endregion
+    }
+
+    // Designer.
+    public sealed partial class stabcontrol {
+        /// <summary>
+        /// Initialize component.
+        /// </summary>
+        public void Init() {
+            #region Base
+
+            DrawMode = TabDrawMode.OwnerDrawFixed;
+            SetStyle(
+                ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw |
+                ControlStyles.UserPaint | ControlStyles.UserMouse,true);
+            BackColor = Color.FromArgb(60,60,60);
+            ForeColor = Color.White;
+
+            #endregion
+        }
+    }
+}
